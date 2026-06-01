c09e1e648dc2e99373becf6c01760b8598a24999
adervalsa <144601630+adervalsa@users.noreply.github.com>
2026-06-01 14:18:25 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+--========================================================--
+--  Script desenvolvido por Piyo Store
+--  Todos os direitos reservados © Piyo Store
+--  Contato: https://discord.gg/Nc6XjRFBnw 
+--========================================================--
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Piyo Store'
+description 'Pacote de recursos personalizados da Piyo Store'
+version '1.0.0'
+
+client_scripts {
+    'ped_config.lua'
+}
+
+files {
+    'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
